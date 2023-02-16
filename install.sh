4f9f488223779b56ee483e2bc3d4761ece6859c7
Luna R <lunarnightskies@gmail.com>
2023-02-16 08:44:05 -0500

    rename

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo MAKE SURE THIS DIRECTORY IS IN YOUR HOME FOLDER AND IS CALLED .shellcfg
-
-ln -sf ~/.shellcfg/.aliases ~/
-ln -sf ~/.shellcfg/.zshrc ~/
-ln -sf ~/.shellcfg/.tmux.conf ~/
