3293af4502901ef3b8d6814d8fa9dd563e13407a
Luna <lunarnightskies@gmail.com>
2023-04-25 16:25:47 +0000

    install zsh syntax highlighting

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+echo "Installing zsh plugins..."
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Installing dotfiles..."
 ln -sf $PWD/.aliases ~/
 ln -sf $PWD/.zshrc ~/
 ln -sf $PWD/.tmux.conf ~/
